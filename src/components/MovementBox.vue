ae40edad0c678ffa3d620b60299fce003c29f17b
Ignacio Pedemonte Berthoud <ipedemonteb@gmail.com>
2024-10-27 11:50:55 -0300

    feat: add transfer detail component

--- src/components/MovementBox.vue
+++ src/components/MovementBox.vue
@@ -20,65 +20,108 @@
 
                     <v-list>
                         <v-list-item>
-                            <v-btn class="detailsButton" prepend-icon="mdi-format-list-bulleted">Detalles</v-btn>
+                            <v-btn class="detailsButton" prepend-icon="mdi-format-list-bulleted" @click="openModal">Detalles</v-btn>
                         </v-list-item>
                     </v-list>
                 </v-menu>
-            </div>   
+            </div>  
+            
+            <div v-if="isModalOpen" class="modalOverlay" @click.self="closeModal">
+                <div class="modalContent">
+                    <TransferInfo 
+                        :closeModal="closeModal"
+                        :cvuRem="cvuRem"
+                        :cvuDest="cvuDest"
+                        :amount="amount"
+                        :date="date"
+                    />
+                </div>
+            </div>
         </div>
-
     </div>
 </template>
 
 <style scoped>
-    .movement {
-        border: 1px solid #ccc;
-        border-radius: 10px;
-        margin: 5px 0px;
-    }
+.movement {
+    border: 1px solid #ccc;
+    border-radius: 10px;
+    margin: 5px 0px;
+}
 
-    .icon {
-        margin-right: 8px;
-    }
+.icon {
+    margin-right: 8px;
+}
 
-    .movementContainer {
-        margin: 12px;
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-    }
+.movementContainer {
+    margin: 12px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
 
-    .iconInfoContainer {
-        display: flex;
-        align-items: center;
-    }
+.iconInfoContainer {
+    display: flex;
+    align-items: center;
+}
 
-    .rightContainer {
-        display: flex;
-        align-items: center;
-    }
+.rightContainer {
+    display: flex;
+    align-items: center;
+}
 
-    .moreOptions {
-        margin-left: 10px;
-    }
+.moreOptions {
+    margin-left: 10px;
+}
 
-    .actionContainer h5 {
-        margin-top: 0px;
-        color: grey;
-    }
+.actionContainer h5 {
+    margin-top: 0px;
+    color: grey;
+}
 
-    .amountContainer {
-        margin-right: 5px;
-    }
+.amountContainer {
+    margin-right: 5px;
+}
 
-    .detailsButton {
-        width: 100%;
-    }
+.detailsButton {
+    width: 100%;
+}
 
+.modalOverlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 1000;
+}
+
+.modalContent {
+    background-color: white;
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2);
+}
 </style>
 
 <script setup>
-    import { defineProps } from 'vue';
-    defineProps(['action', 'source', 'amount', 'timeAgo']);
-</script>
+import { ref, defineProps } from 'vue';
+import TransferInfo from './TransferInfo.vue';
 
+defineProps(['action', 'source', 'amount', 'timeAgo']);
+
+const isModalOpen = ref(false);
+const cvuRem = ref('823742374283472834');
+const cvuDest = ref('1234567890123456789012');
+const date = ref('12/12/2021');
+
+function openModal() {
+  isModalOpen.value = true;
+}
+function closeModal() {
+  isModalOpen.value = false;
+}
+</script>
